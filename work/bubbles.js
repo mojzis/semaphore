8be125bde422cc11931d3e34cd5c9ac1a763e25c
bkai <bkai@email.cz>
2013-04-06 23:08:48 +0200

    Bubbles shapes

---
+++ work/bubbles.js
@@ -0,0 +1,16 @@
+var bubbles = [
+    "-114,-11|-134,-1|-160,29|-122,40|-158,76|-166,78",
+    "-46,5|-61,12|-77,20|-68,47|-38,48|-35,54",
+    "-79,14|-87,26|-109,71|-48,46|-66,85|-72,91",
+    "-4,22|-11,33|1,49|-20,47|-9,59|-6,62"
+];
+
+//<actor t="translate(71,19) rotate(-2)" pose="-11,9|-1,114|-11,99|-11,89|-11,79|-11,59|-16,34|-21,9|-6,34|-1,9|-18,79|-18,59|-6,79|13,83">
+//    <bubble t="translate(-3,0)" pose="0,0|-12,22|-83,104|45,21|-37,182|-58,162">
+//        <tspan x="0" y="-2em">Yes!
+//            <tspan x="0" y="0em">people would use</tspan>
+//            <tspan x="0" y="1em">simple HTML markup</tspan>
+//            <tspan x="0" y="2em">and collaborate</tspan>
+//            <tspan x="0" y="3em">on <tspan fill="blue">github</tspan></tspan>
+//        </tspan></bubble>
+//</actor>
