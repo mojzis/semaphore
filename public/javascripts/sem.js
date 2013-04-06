7e88284c9a28c14c2e2089b1f4a3943979e2fc96
mojzis <mojzisek@gmail.com>
2013-04-06 19:15:35 +0000

    added markdown, tweet this

--- public/javascripts/sem.js
+++ public/javascripts/sem.js
@@ -3,30 +3,35 @@ var serverAddress = "http://mojzis.apiary.dev:8000/message";
 // check location.hash, in case it contains an id, request the stuff on server and paint it blue
 if(location.hash) {
   var messId = location.hash.substr(1);
-    var request = $.ajax({
-      url: serverAddress + '/'+ messId,
-      type: "get",
-      complete: function(event) {
-        var response = $.parseJSON(event.responseText);
-        drawSemaphore(response.actorPoses);
-        }
-      });
+  var request = $.ajax({
+    url: serverAddress + '/'+ messId,
+    type: "get",
+    complete: function(event) {
+      $('#teaser').html('send a reply :');
+      var response = $.parseJSON(event.responseText);
+      drawSemaphore(response.actorPoses);
+    }
+  });
 
 }
 $('#sendmess').click(function(){
-    var mess = $('#mess').val();
-    var request = $.ajax({
-      url: serverAddress,
-      type: "post",
-      data: { "text": mess },
-      complete: function(event) {
-        var response = $.parseJSON(event.responseText);
-        // TODO: write down the link
-        $('#notif').html('share: <a href="' + location.origin + '#' + response.id + '">' + mess + '</a>');
-        // TODO: add a tweet this button :)
-        drawSemaphore(response.actorPoses);
-        }
-      });
+  var mess = $('#mess').val();
+  var request = $.ajax({
+    url: serverAddress,
+    type: "post",
+    data: { "text": mess },
+    complete: function(event) {
+      var response = $.parseJSON(event.responseText);
+      // TODO: write down the link
+      var url = location.origin + '#' + response.id;
+      $('#notif').html('share: <a href="' + url + '">' + mess + '</a><a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>');
+
+      // TODO: add a tweet this button :)
+      $('.twitter-share-button').attr('data-url', url).attr('data-text','#CMXore');
+      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+      drawSemaphore(response.actorPoses);
+      }
+    });
   });
 });
 
@@ -36,9 +41,9 @@ function drawSemaphore(poses) {
   var sceneEnd = "</scene>";
   var actorTemplate = $("<actor></actor>");
   var actors = [];
-  var actorWidth = 100;
+  var actorWidth = 125;
   for (j = 0; j < poses.length; j++) {
-    actors.push(actorTemplate.attr('pose',poses[j]).attr('t','translate(' + (60 +(j) * actorWidth) + ',150)').clone());
+    actors.push(actorTemplate.attr('pose',poses[j]).attr('t','translate(' + (65 +(j) * actorWidth) + ',150)').clone());
   }
   console.log(actors);
   sceneHtml = sceneStart;
@@ -62,3 +67,5 @@ function drawSemaphore(poses) {
   }
 
   }
+
+
