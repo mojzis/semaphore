5dd866adc868cb33855d08ec6bf000a30f60a615
mojzis <mojzisek@gmail.com>
2013-04-06 12:24:49 +0000

    error

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.OPENSHIFT_INTERNAL_PORT || 3000);
+app.set('IP', process.env.OPENSHIFT_INTERNAL_IP || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -32,6 +33,6 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/ahoj', ahoj.greet);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), app.get('IP'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
